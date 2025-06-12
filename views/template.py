37bc409a05370a3dffc2f7e061ca019597b40966
VitoBonetti <vitobonetti@gmail.com>
2025-06-12 20:52:02 +0200

    check update

--- views/template.py
+++ views/template.py
@@ -9,7 +9,8 @@ class Template:
         self.state = state
         self.content_view = content_view
         self.selected_index = selected_index
-        self.check_update_icon = ft.Icon(name=ft.Icons.VERIFIED_OUTLINED, tooltip="Up to date", color=ft.Colors.GREEN, size=20)
+        self.check_update_icon = ft.Icon(name=ft.Icons.VERIFIED_OUTLINED, tooltip="Up to date", color=ft.Colors.GREEN,
+                                         size=20)
         state.check_update_icon = self.check_update_icon
         self.info_progress = ft.ProgressRing(
             color=ft.Colors.ORANGE,
